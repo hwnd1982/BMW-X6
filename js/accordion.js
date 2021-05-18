df68e9c14cac69ae76045ffda79e94c7910f02aa
Kirill Lavrov <lkv@myttk.ru>
2021-05-19 00:33:08 +0700

    create: replaced condition

--- js/accordion.js
+++ js/accordion.js
@@ -6,20 +6,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
   featureLinkElems.forEach((btn, index) => {
     btn.addEventListener('click', () => {
-      
-      featureSubElems.forEach((featureSubElem, i) => {
-        if (i!=index) {
+      if (btn.classList.contains('feature__link_active')) {
+        featureSubElems[index].classList.add('hidden');
+        btn.classList.remove('feature__link_active');  
+      } else {
+        featureSubElems.forEach((featureSubElem, i) => {
           featureSubElem.classList.add('hidden');
-        }
-      });
-      featureLinkElems.forEach((featureLinkElem, i) => {
-        if (i!=index) {
+        });
+        featureLinkElems.forEach((featureLinkElem, i) => {
           featureLinkElem.classList.remove('feature__link_active');
-        }
-      });
-      
-      featureSubElems[index].classList.toggle('hidden');
-      btn.classList.toggle('feature__link_active');
+        });
+        featureSubElems[index].classList.toggle('hidden');
+        btn.classList.toggle('feature__link_active');
+      }
     })
   })
 });
